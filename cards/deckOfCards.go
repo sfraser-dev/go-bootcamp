872f0e6649b3b2cdf07b3e1f3f4f0547d21c115d
Jack Edge <js77js@yahoo.co.uk>
2023-12-13 14:50:30 +0000

    some error handling

--- cards/deckOfCards.go
+++ cards/deckOfCards.go
@@ -1,11 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // (""like a class"")
 // create a new type of "deck" which is a slice of string
 type deckOfCards []string
 
+// (""like a method"")
+// receivers sets up methods on variables that we create. (d deck) is a receiver
+func (doc deckOfCards) print() {
+	fmt.Print("---------------------\n")
+	for i, card := range doc {
+		fmt.Printf("%02v: %17s\n", i+1, card)
+	}
+	fmt.Print("\n")
+}
+
+// (""like a method"")
+func (doc deckOfCards) deckOfCardsToSingleString() string {
+	stringOut := strings.Join(doc, ",")
+	return stringOut
+}
+
+// (""like a method"")
+func (doc deckOfCards) writeToFile(filename string) {
+	err := os.WriteFile(filename, []byte(doc.deckOfCardsToSingleString()), 0666)
+	if err != nil {
+		fmt.Println("Error writing", filename, ": ", err)
+		os.Exit(1)
+	}
+}
+
+// (""like a method"")
+func (doc deckOfCards) readFromFile(filename string) string {
+	byteSlice, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading ", filename, ": ", err)
+		os.Exit(1)
+	}
+	return string(byteSlice)
+}
+
+// helper function
 func newDeckOfCards() deckOfCards {
 	cards := deckOfCards{}
 	suits := [4]string{"Clubs", "Diamonds", "Hearts", "Spades"}
@@ -19,11 +59,7 @@ func newDeckOfCards() deckOfCards {
 	return cards
 }
 
-// (""like a method"")
-// receivers sets up methods on variables that we create. (d deck) is a receiver
-func (dc deckOfCards) print() {
-	for i, card := range dc {
-		fmt.Printf("%2v: %17s\n", i, card)
-	}
-	fmt.Print("\n")
-}
+// helper function
+// func dealCards(d deckOfCards, handSize int) (deckOfCards, deckOfCards) {
+// 	return d[:handSize], d[handSize:]
+// }
--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "bitbucket.org/weebucket/gobootcamp"
+import (
+	"fmt"
+
+	"bitbucket.org/weebucket/gobootcamp"
+)
 
 func main() {
-	var cards deckOfCards = newDeckOfCards()
-	cards.print()
+	// of type deckOfCards
+	cards := newDeckOfCards()
+	// cards.print()
+	
+	// hand, remainingCards := dealCards(cards, 5)
+	// hand.print()
+	// remainingCards.print()
 
+	// save to file
+	cards.writeToFile("mycards.log")
+	strRead:= cards.readFromFile("mycards.log")
+	fmt.Println(strRead)
+
+	// testing
 	gobootcamp.Saytop()
+	greetingStr := "Hello"
+	byteSlice := ([]byte(greetingStr))
+	fmt.Println(byteSlice)
+	fmt.Println(string(byteSlice))
+
 }
