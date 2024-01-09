1992750928f31fc87a614201a0ebf0baf8adca69
Jack Edge <js77js@yahoo.co.uk>
2024-01-09 16:42:11 +0000

    more progress on go routines and channels

--- parallel/main.go
+++ parallel/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -22,22 +23,29 @@ func main() {
 		go checkLink(website, c)
 	}
 
-	// pause and wait for channel data
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	// recieve all the channel messages(iterate over the channel)
+	//for { go checkLink(<-c, c) }
+	// refactor: wait for the channel to return some value, assign to ws
+	//for ws := range c { go checkLink(ws, c) }
+	// refactor to use function literal (lambda / anonymous function)
+	for ws := range c {
+		go func() {
+			checkLink(ws, c)
+			time.Sleep(1 * time.Second)
+		}()
+	}
 }
 
 func checkLink(website string, c chan string) {
 	_, err := http.Get(website)
 	if err != nil {
-		msg := fmt.Sprintf("%s: %s", website, err)
-		c <- msg
+		msg := fmt.Sprintf("%s: %s\n", website, err)
+		fmt.Print(msg)
+		c <- website
 		return
 	}
 	// io.Copy(os.Stdout, resp.Body)
-	msg := fmt.Sprintf("%s: yip, it's up", website)
-	c <- msg
+	msg := fmt.Sprintf("%s: yip, it's up\n", website)
+	fmt.Print(msg)
+	c <- website
 }
