9179c3c6c198bcf31d636c7874d0e94286fb27ea
Jack Edge <js77js@yahoo.co.uk>
2024-01-10 11:08:36 +0000

    course completed

--- parallel/main.go
+++ parallel/main.go
@@ -23,16 +23,18 @@ func main() {
 		go checkLink(website, c)
 	}
 
-	// recieve all the channel messages(iterate over the channel)
+	/* recieve all the channel messages(iterate over the channel) */
 	//for { go checkLink(<-c, c) }
-	// refactor: wait for the channel to return some value, assign to ws
+	/* refactor: wait for the channel to return some value, assign to ws */
 	//for ws := range c { go checkLink(ws, c) }
-	// refactor to use function literal (lambda / anonymous function)
+	/* refactor to use function literal (lambda / anonymous function) -
+	function literals are handy in go routine invocations (as Go Routines
+	must operate on a function) */
 	for ws := range c {
-		go func() {
-			checkLink(ws, c)
-			time.Sleep(1 * time.Second)
-		}()
+		go func(wsFuncLit string) {
+			time.Sleep(2 * time.Second)
+			checkLink(wsFuncLit, c)
+		}(ws) /* note the parentheses - must call the function */
 	}
 }
 
